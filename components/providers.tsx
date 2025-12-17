dbf36c1e3a5ce8f8082d1cc5f58a871eeaadf4c5
nmatunog <nmatunog@gmail.com>
2025-12-17 16:02:57 +0800

    TEMP: Bypass AuthProvider to test if it's causing 500 error
    - Temporarily disable AuthProvider to isolate the issue
    - If setup page works now, the issue is in AuthProvider
    - Will re-enable after fixing

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,9 +1,13 @@
 'use client';
 
-import { AuthProvider } from '@/contexts/auth-context';
+// Temporarily bypassing AuthProvider to test if it's causing the 500 error
+// import { AuthProvider } from '@/contexts/auth-context';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  // TEMPORARY: Bypassing AuthProvider to test if it's causing the 500 error
+  // TODO: Re-enable AuthProvider after fixing the issue
+  return <>{children}</>;
+  // return <AuthProvider>{children}</AuthProvider>;
 }
 
 
